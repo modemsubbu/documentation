22c824d0b6bdbebdf348c860a36a651026dc51b4
nani <subbumodem@gmail.com>
2017-12-06 11:50:06 +0530

    nothing

--- login.java
+++ login.java
@@ -2,10 +2,10 @@ import java.util.*
 class Nani
 {
 	public static void main(String args[]);
-	hi
-	int i=100000;
+	int i=100000000000000;
 	hi all
 		hello
+		hai hello
 	System.out.println("hello welcome to all");
 
 }
